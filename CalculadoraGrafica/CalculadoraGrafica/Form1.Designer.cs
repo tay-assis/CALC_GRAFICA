a54227e4f0ac9d09c24d3de295e49068441f2f2e
Taynara Araújo <112909056+tay-assis@users.noreply.github.com>
2024-01-09 14:53:27 -0300

    Implementações de alguns botões funcionando

--- CalculadoraGrafica/CalculadoraGrafica/Form1.Designer.cs
+++ CalculadoraGrafica/CalculadoraGrafica/Form1.Designer.cs
@@ -29,22 +29,113 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Numero_1 = new System.Windows.Forms.Button();
+            this.Numero_2 = new System.Windows.Forms.Button();
+            this.Botao_Soma = new System.Windows.Forms.Button();
+            this.Exibir_Num = new System.Windows.Forms.TextBox();
+            this.Botao_Igual = new System.Windows.Forms.Button();
+            this.Numero_3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Numero_1
+            // 
+            this.Numero_1.Font = new System.Drawing.Font("Mongolian Baiti", 25.92F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero_1.Location = new System.Drawing.Point(12, 319);
+            this.Numero_1.Margin = new System.Windows.Forms.Padding(4);
+            this.Numero_1.Name = "Numero_1";
+            this.Numero_1.Size = new System.Drawing.Size(60, 60);
+            this.Numero_1.TabIndex = 0;
+            this.Numero_1.Text = "1";
+            this.Numero_1.UseVisualStyleBackColor = true;
+            this.Numero_1.Click += new System.EventHandler(this.Numero_1_Click);
+            // 
+            // Numero_2
+            // 
+            this.Numero_2.Font = new System.Drawing.Font("Mongolian Baiti", 25.92F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero_2.Location = new System.Drawing.Point(80, 319);
+            this.Numero_2.Margin = new System.Windows.Forms.Padding(4);
+            this.Numero_2.Name = "Numero_2";
+            this.Numero_2.Size = new System.Drawing.Size(60, 60);
+            this.Numero_2.TabIndex = 2;
+            this.Numero_2.Text = "2";
+            this.Numero_2.UseVisualStyleBackColor = true;
+            this.Numero_2.Click += new System.EventHandler(this.Numero_2_Click);
+            // 
+            // Botao_Soma
+            // 
+            this.Botao_Soma.Font = new System.Drawing.Font("Mongolian Baiti", 28.224F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botao_Soma.Location = new System.Drawing.Point(344, 312);
+            this.Botao_Soma.Margin = new System.Windows.Forms.Padding(4);
+            this.Botao_Soma.Name = "Botao_Soma";
+            this.Botao_Soma.Size = new System.Drawing.Size(75, 127);
+            this.Botao_Soma.TabIndex = 11;
+            this.Botao_Soma.Text = "+";
+            this.Botao_Soma.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Botao_Soma.UseVisualStyleBackColor = true;
+            this.Botao_Soma.Click += new System.EventHandler(this.Botao_Soma_Click);
+            // 
+            // Exibir_Num
+            // 
+            this.Exibir_Num.Font = new System.Drawing.Font("Mongolian Baiti", 47.808F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exibir_Num.Location = new System.Drawing.Point(12, 12);
+            this.Exibir_Num.Multiline = true;
+            this.Exibir_Num.Name = "Exibir_Num";
+            this.Exibir_Num.Size = new System.Drawing.Size(408, 96);
+            this.Exibir_Num.TabIndex = 15;
+            this.Exibir_Num.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Exibir_Num.TextChanged += new System.EventHandler(this.Exibir_Num_TextChanged);
+            // 
+            // Botao_Igual
+            // 
+            this.Botao_Igual.Font = new System.Drawing.Font("Microsoft Yi Baiti", 36.288F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botao_Igual.Location = new System.Drawing.Point(277, 380);
+            this.Botao_Igual.Name = "Botao_Igual";
+            this.Botao_Igual.Size = new System.Drawing.Size(60, 59);
+            this.Botao_Igual.TabIndex = 16;
+            this.Botao_Igual.Text = "=";
+            this.Botao_Igual.UseVisualStyleBackColor = true;
+            this.Botao_Igual.Click += new System.EventHandler(this.Botao_Igual_Click);
+            // 
+            // Numero_3
+            // 
+            this.Numero_3.Font = new System.Drawing.Font("Mongolian Baiti", 25.92F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero_3.Location = new System.Drawing.Point(148, 319);
+            this.Numero_3.Margin = new System.Windows.Forms.Padding(4);
+            this.Numero_3.Name = "Numero_3";
+            this.Numero_3.Size = new System.Drawing.Size(60, 60);
+            this.Numero_3.TabIndex = 3;
+            this.Numero_3.Text = "3";
+            this.Numero_3.UseVisualStyleBackColor = true;
+            this.Numero_3.Click += new System.EventHandler(this.Numero_3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 382);
+            this.ClientSize = new System.Drawing.Size(432, 452);
+            this.Controls.Add(this.Numero_3);
+            this.Controls.Add(this.Botao_Igual);
+            this.Controls.Add(this.Exibir_Num);
+            this.Controls.Add(this.Botao_Soma);
+            this.Controls.Add(this.Numero_2);
+            this.Controls.Add(this.Numero_1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Calculadora";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Numero_1;
+        private System.Windows.Forms.Button Numero_2;
+        private System.Windows.Forms.Button Botao_Soma;
+        private System.Windows.Forms.TextBox Exibir_Num;
+        private System.Windows.Forms.Button Botao_Igual;
+        private System.Windows.Forms.Button Numero_3;
     }
 }
 
--- CalculadoraGrafica/CalculadoraGrafica/Form1.cs
+++ CalculadoraGrafica/CalculadoraGrafica/Form1.cs
@@ -7,14 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CalculadoraGrafica
 {
     public partial class Form1 : Form
     {
+        // Variáveis globais
+        private double num1 = 0;
+        private double num2 = 0;
+        private string operador = "";
+        private double resultado = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Desabilitando o TextBox para edição e seleção
+            Exibir_Num.ReadOnly = true;
+            Exibir_Num.Enabled = false;
+            Exibir_Num.Text = resultado.ToString("0");
         }
+
+        // Verifica se a operação foi selecionada
+        private bool VerificaOperacao()
+        {
+            return string.IsNullOrEmpty(operador); // Retorna true se a operação não foi selecionada
+        }
+
+        private void AtualizarExibirNum(string texto)
+        {
+            Exibir_Num.Text += texto; // Atualiza o TextBox com o número digitado
+        }
+
+        private void LimparTextBox()
+        {
+            Exibir_Num.Clear(); // Limpa o TextBox
+        }   
+
+        // Método que atualiza as variáveis num1 e num2 com o número 1
+        public void Numero_1_Click(object sender, EventArgs e)
+        {
+            LimparTextBox(); // Limpa o TextBox
+            System.Windows.Forms.Button botao = (System.Windows.Forms.Button)sender; // Captura o botão que foi clicado
+            string texto_Botao = botao.Text; // Captura o texto do botão
+
+            if(VerificaOperacao())
+            {
+                // Se não foi realizada nenhuma operação, os números vão para num1
+                AtualizarExibirNum(texto_Botao); // Atualiza o TextBox com o número digitado
+                double.TryParse(Exibir_Num.Text, out num1); // Armazena num1
+            }
+            else
+            {
+                AtualizarExibirNum(texto_Botao); // Atualiza o TextBox com o número digitado
+                double.TryParse(Exibir_Num.Text, out num2); // Armazena num2
+            }
+        }
+
+        // Método que atualiza as variáveis num1 e num2 com o número 2
+        public void Numero_2_Click(object sender, EventArgs e)
+        {
+            LimparTextBox(); // Limpa o TextBox
+            System.Windows.Forms.Button botao = (System.Windows.Forms.Button)sender; // Captura o botão que foi clicado
+            string texto_Botao = botao.Text; // Captura o texto do botão
+
+            if (VerificaOperacao())
+            {
+                // Se não foi realizada nenhuma operação, os números vão para num1
+                AtualizarExibirNum(texto_Botao); // Atualiza o TextBox com o número digitado
+                double.TryParse(Exibir_Num.Text, out num1); // Armazena num1
+            }
+            else
+            {
+                AtualizarExibirNum(texto_Botao); // Atualiza o TextBox com o número digitado
+                double.TryParse(Exibir_Num.Text, out num2); // Armazena num2
+            }
+        }
+
+        // Método que atualiza as variáveis num1 e num2 com o número 3
+        private void Numero_3_Click(object sender, EventArgs e)
+        {
+            LimparTextBox(); // Limpa o TextBox
+            System.Windows.Forms.Button botao = (System.Windows.Forms.Button)sender; // Captura o botão que foi clicado
+            string texto_Botao = botao.Text; // Captura o texto do botão
+
+            if (VerificaOperacao())
+            {
+                AtualizarExibirNum(texto_Botao);
+                double.TryParse(Exibir_Num.Text, out num1);
+            }
+            else
+            {
+                AtualizarExibirNum(texto_Botao);
+                double.TryParse(Exibir_Num.Text, out num2);
+            }
+        }
+
+        public void Botao_Soma_Click(object sender, EventArgs e)
+        {
+            // Captura o valor do primeiro número inserido
+            if (double.TryParse(Exibir_Num.Text, out num1))
+            {
+                operador = "+"; // Define a operação como soma
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        public void Exibir_Num_TextChanged(object sender, EventArgs e)
+        {
+            const int maximo_Caracteres = 10; // Define o número máximo de caracteres
+
+            if (Exibir_Num.Text.Length > maximo_Caracteres)
+            {
+                // Se o número de caracteres exceder o máximo, remova o último caractere
+                Exibir_Num.Text = Exibir_Num.Text.Substring(0, maximo_Caracteres);
+            }
+        }
+
+        private void Botao_Igual_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(Exibir_Num.Text, out num2))
+            {
+                switch (operador)
+                {
+                    case "+":
+                        resultado = num1 + num2;
+                        break;
+                    // outras operações aqui...
+
+                    default:
+                        break;
+                }
+
+                Exibir_Num.Text = resultado.ToString();
+                LimparTextBox(); // Limpa o TextBox
+                Exibir_Num.Text = resultado.ToString("0");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        
     }
 }
